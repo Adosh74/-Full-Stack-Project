4cb2fe786b1af7fcd40dea3c6a6cd06839c34492
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-02-18 14:19:28 +0200

    create App and Header component

---
+++ src/components/app.tsx
@@ -0,0 +1,12 @@
+import Header from "./header";
+
+const App = () => {
+    
+    return ( 
+    <div className= "container">
+        <Header message = "TEST" />
+    </div> 
+)
+};
+
+export default App;
